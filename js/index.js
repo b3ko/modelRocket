6bafd7329c43396605f1db0af1f46a9e64fb3766
Jon <berkowitz.jon@gmail.com>
2016-05-10 00:18:28 -0400

    adding additional logic to js. needs to be completed and greatly cleaned up. see in line comments

--- js/index.js
+++ js/index.js
@@ -6,6 +6,8 @@ seconds = 00;
 interval = null;
 isRunning = true; //used to pause the clock in the event of a hold or abort
 direction = -1;  //for count down this is negative - post launch it will be positive
+holdCount = 0;
+abortCount = 0;
 
 //wait for doc and then GO!
 $(document).ready(function() {
@@ -39,47 +41,90 @@ function countdown() {
       direction = 1;
       $("#tminus").text("+");
     }
-};
+  };
 
 
 //a button was clicked on the checklist so update buttons, status, and clock accordingly
- $('.btn').click(function() {  //this function seems very repetitive -- try to refactor!
+ $('.btn').click(function() {  
 
+/*****************************
+ //this function seems very repetitive -- NEEDS TO BE refactored!
+
+  //bug - if you have an abort and a hold selected in different rows, and then clear all the aborts the clock stays red.
+  need to add logic to go with highest level of warning (so green only when all green, orange only when no reds, 
+  and when red is all cleared check for orange before setting to green.
+  but don't stay on red.)
+
+
+  refactor ideas:
+  1) break into smaller functions
+  2) take code that is repeated (other than colors that are used) and pass in a parameter that will grab the approriate values from an array.
+    possible issues with this is that each button behaves slightly different. too tired to think straight. bedtime.
+
+*************/
     //a GO was clicked
     if ($(this).hasClass("btn-success")) {
       $(this).closest(".row").find(".btn").css("background-color", "#555555"); //update the other buttons in this row
       $(this).css("background-color", "#00ff00"); //update the GO button
       $(this).css("border-color", "white");
-      if(!isRunning) {  //if the clock was stopped start it again
-        isRunning = true;
-        interval = setInterval(countdown, 1000);
+      if(abortCount > 0) {
+        if( $(this).closest(".row").find(".btn").hasClass("abort") ) {
+          $(this).closest(".row").find(".btn").removeClass("abort");
+          abortCount--;
+        }
       }
-      $("#clock").css("color", "#00ff00"); //return the clock to GO colors
-      $("#clock").css("border-color", "#00ff00");
-      $("#run").text("RUNNING");  //update the status
-      $("#run").css("color","#00ff00");
-      $("#run").css("background-color","#111111");
-      $("#run").css("border-color","white");
+      if(holdCount > 0) {
+        if( $(this).closest(".row").find(".btn").hasClass("hold") ) {
+          $(this).closest(".row").find(".btn").removeClass("hold");
+          holdCount--;
+        }
+      }
+      if(abortCount == 0 && holdCount == 0) { //only start the clock and update status if all holds and aborts are cleared
+        if(!isRunning) {  //if the clock was stopped start it again
+          isRunning = true;
+          interval = setInterval(countdown, 1000);
+        }
+        $("#clock").css("color", "#00ff00"); //return the clock to GO colors
+        $("#clock").css("border-color", "#00ff00");
+        $("#run").text("RUNNING");  //update the status
+        $("#run").css("color","#00ff00");
+        $("#run").css("background-color","#111111");
+        $("#run").css("border-color","white");
+      }
     }
 
     //a HOLD was clicked ...update the button, the rest of row, clock, stop the clock, update the status
     if ($(this).hasClass("btn-warning")) {
-      $(this).closest(".row").find(".btn").css("background-color", "#555555");
-      $(this).css("background-color", "#ffe600");
-      $(this).css("border-color", "white");
-      clearInterval(interval);
-      isRunning = false;
-      $("#clock").css("color", "#ffe600");
-      $("#clock").css("border-color", "#ffe600");
-      $("#run").text("HOLD");
-      $("#run").css("color","#ffe600");
-      $("#run").css("background-color","#111111");
-      $("#run").css("border-color","#ffe600");
+      if( $(this).closest(".row").find(".btn").hasClass("hold"))
+        { }
+      else {
+        holdCount++;
+        $(this).closest(".row").find(".btn").css("background-color", "#555555");
+        $(this).closest(".row").find(".btn").addClass("hold");
+        $(this).css("background-color", "#ffe600");
+        $(this).css("border-color", "white");
+        clearInterval(interval);
+        isRunning = false;
+        if (abortCount == 0) {
+          $("#clock").css("color", "#ffe600");
+          $("#clock").css("border-color", "#ffe600");
+          $("#run").text("HOLD");
+          $("#run").css("color","#ffe600");
+          $("#run").css("background-color","#111111");
+          $("#run").css("border-color","#ffe600");
+        }
+      }
     }
 
     //an abort was clicked ...update the button, the rest of row, clock, stop the clock, update the status
     if ($(this).hasClass("btn-danger")) {
+      if( $(this).closest(".row").find(".btn").hasClass("abort") )
+        { }
+      else {
+        abortCount++;
+      }
       $(this).closest(".row").find(".btn").css("background-color", "#ff0000");
+      $(this).closest(".row").find(".btn").addClass("abort");
       $(this).css("background-color", "#ff0000");
       $(this).css("border-color", "white");
       clearInterval(interval);
